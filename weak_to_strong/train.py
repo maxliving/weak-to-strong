67dad8e58b4ed9b6d0630c2110771787115016bf
Max Livingston <maxliving@gmail.com>
2025-12-06 12:07:51 -0500

    safe serialization to false

--- weak_to_strong/train.py
+++ weak_to_strong/train.py
@@ -269,7 +269,7 @@ def train_and_save_model(
         if save_path:
             # Note: If the model is wrapped by DataParallel, we need to unwrap it before saving
             (model if hasattr(model, "save_pretrained") else model.module).save_pretrained(
-                save_path
+                save_path, safe_serialization=False
             )
             print("saved", save_path)
 
